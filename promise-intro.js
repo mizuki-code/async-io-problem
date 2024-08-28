8a6885c1d3756371e9623786b3e943e58882d12e
mizuki-code <dogwooood@gmail.com>
2024-08-29 01:21:01 +0900

    非同期I/OでのPromiseオブジェクトによる制御を実装

---
+++ promise-intro.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const https = require('node:https');
+const fs = require('node:fs');
+let data = '';
+
+const promise = new Promise((resolve, reject) => {
+    https.get('https://www.nicovideo.jp/ranking/genre/all?term=hour&rss=2.0&lang=ja-jp',
+      {
+        headers: {
+          'User-Agent': 'node',
+        },
+      },
+      (response) => {
+        response
+          .on('data', (chunk) => {
+            data += chunk;
+          })
+          .on('end', () => {
+            resolve();
+          });
+      }
+    );
+  });
+
+  promise.then(() => {
+    fs.writeFile('test.txt', data, 'utf8', () => {
+    });
+  });
